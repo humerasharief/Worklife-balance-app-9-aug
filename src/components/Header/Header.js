3657762b046f6cb5827789f6a761c3ba303c389b
Humera Yasmeen <humerasharief1906@gmail.com>
2023-08-07 17:51:16 +0530

    Updated the global styles & placement

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,34 +8,37 @@ import Introduction from '../Introduction/Introduction';
 const Header = () => {
     const location = useLocation();
   return (
-    <header className="header">
-      <h1>Work Life Balancer Application </h1>
-      {location.pathname !== '/' ?  
-        (<nav className="navigation"  >
-          <ul className="nav-list">
-            <li className={location.pathname === '/intro' ? 'active' : ''}>
-              <NavLink exact to="/intro">
-                Introduction
-              </NavLink>
-            </li>
-            <li className={location.pathname === '/home' ? 'active' : ''}>
-              <NavLink exact to="/home">
-                Dashboard
-              </NavLink>
-            </li>
-            <li className={location.pathname === '/analytics' ? 'active' : ''}>
-              <NavLink exact to="/analytics">
-                Analytics
-              </NavLink>
-            </li>
-          </ul>
-        </nav>) : null}
+    <>
+      <header className="header">
+        <h1>Work Life Balancer Application </h1>
+      
+        {location.pathname !== '/' ?  
+          (<nav className="navigation"  >
+            <ul className="nav-list">
+              <li className={location.pathname === '/intro' ? 'active' : ''}>
+                <NavLink exact to="/intro">
+                  Home
+                </NavLink>
+              </li>
+              <li className={location.pathname === '/home' ? 'active' : ''}>
+                <NavLink exact to="/home">
+                  Dashboard
+                </NavLink>
+              </li>
+              <li className={location.pathname === '/analytics' ? 'active' : ''}>
+                <NavLink exact to="/analytics">
+                  Analytics
+                </NavLink>
+              </li>
+            </ul>
+          </nav>) : null}
+        </header>
         <Switch>
           <Route exact path="/" component={Login} />
           <Route exact path="/intro" component={Introduction} />
           <Route exact path="/analytics" component={Analytics} />
-        </Switch>
-    </header>
+        </Switch>      
+    </>
   );
 };
 
